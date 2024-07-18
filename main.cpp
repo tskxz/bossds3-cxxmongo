3d2abdd41d17629910e33348ef61a7dc06b710fa
Tanjil <tanjilkh@gmail.com>
2024-07-18 12:41:32 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+#include <bsoncxx/builder/basic/document.hpp>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
+
+using bsoncxx::builder::basic::kvp;
+using bsoncxx::builder::basic::make_array;
+using bsoncxx::builder::basic::make_document;
