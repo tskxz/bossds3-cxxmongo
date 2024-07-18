8b1184888b1fc6791cc9b85b8caaa269860b0eb9
Tanjil <tanjilkh@gmail.com>
2024-07-18 14:15:44 +0100

    readme :memo:

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Compile with: c++ --std=c++11 tutorial.cpp $(pkg-config --cflags --libs libmongocxx)
+// Compile with: c++ --std=c++11 main.cpp $(pkg-config --cflags --libs libmongocxx)
 
 // The following is a formatted copy from the tutorial
 // https://www.mongodb.com/docs/languages/cpp/drivers/current/tutorial/.
